4c488a511497d6ce526589486e6bd31c06107379
wassupnari <wassupnari@gmail.com>
2016-11-10 21:41:38 -0500

    Added image assets to the view

--- SimpleTinder/CardsViewController.swift
+++ SimpleTinder/CardsViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    @IBOutlet weak var navImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
